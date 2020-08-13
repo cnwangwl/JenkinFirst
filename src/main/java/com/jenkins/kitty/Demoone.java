5a73ae1be4aabc5797b3308d8481a7f8a8a2e7d3
kittywang <1006194546@qq.com>
2020-08-13 18:39:44 +0800

    Initial commit

---
+++ src/main/java/com/jenkins/kitty/Demoone.java
@@ -0,0 +1,8 @@
+package com.jenkins.kitty;
+
+public class Demoone {
+    public int intAdd(int a, int b){
+        int r = a + b;
+        return r;
+    }
+}
---
+++ src/test/java/com/kittyjenkins/test/UnitTest.java
@@ -0,0 +1,18 @@
+package com.kittyjenkins.test;
+
+import com.jenkins.kitty.Demoone;
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.Random;
+
+public class UnitTest {
+    @Test
+    public void testAddInt(){
+        int a = new Random().nextInt(1000);
+        int b = new Random().nextInt(2000);
+
+        int c = a + b;
+        int r = new Demoone().intAdd(a, b);
+        Assert.assertTrue("检查intAdd(a, b)函数返回值是否正确", c == r);
+    }
+}
